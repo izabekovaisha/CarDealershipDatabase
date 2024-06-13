f191b73efbb39ff7ddab733c43b2d81a915b5e5a
izabekovaisha <izabekovaisha@gmail.com>
2024-06-12 23:53:13 -0400

    Add 2_script

---
+++ 2_script.sql
@@ -0,0 +1,5 @@
+SELECT d.name AS dealership_name, v.*
+FROM dealerships d
+INNER JOIN inventory i ON d.dealership_id = i.dealership_id
+INNER JOIN vehicles v ON i.VIN = v.VIN
+WHERE d.name = 'D & B Used Cars';
